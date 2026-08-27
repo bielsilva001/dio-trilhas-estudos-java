ae6f8d0d75797692039dd3734994d1b7da908557
Gabriel Silva <gabriel.jrsilva007@gmail.com>
2026-08-26 22:23:55 -0300

    feat: refatorado a classe usuario para podermos adicionar variavel e funcionalidade.

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,5 +1,22 @@
 package model;
 
 public class Usuario {
-    
+    // Atributos da classe Usuario
+    private String nome;
+    private int matricula;
+
+    // Adicionando o construtor da classe
+    public Usuario(String nome, int matricula) {
+        this.nome = nome;
+        this.matricula = matricula;
+    }
+
+    // Adicionando os metodos Getters (leitura)
+    public String getNome() {
+        return nome;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
 }

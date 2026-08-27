31edc91714af94d1def946663b69712b77ca20d0
Gabriel Silva <gabriel.jrsilva007@gmail.com>
2026-08-26 22:17:19 -0300

    feat: Modificado a classe Livro para uma classe funcional, adicionando atributos, construtor e metodos

--- src/model/Livro.java
+++ src/model/Livro.java
@@ -1,7 +1,34 @@
 package model;
 
 public class Livro {
-    public static void main (String[] args) {
-        System.out.println("Hello, World!");
+   
+    // Adicionando atributos da classe Livro
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+    // Adicionando o constutor da classe
+
+    public Livro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true; // Por padrão, todo livro novo nasce disponivel
+    }
+
+    // Métodos Getters e Setters (Ação para leitura e escrita)
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public boolean isDisponivel() {
+        return disponivel;
+    }
+
+    public void setDisponivel(boolean disponivel) {
+        this.disponivel = disponivel;
     }
 }

80725e8b9fc2c4b823e4cb26abd51ca5fea61651
Gabriel Silva <gabriel.jrsilva007@gmail.com>
2026-08-26 22:52:17 -0300

    feat: Adicionei funçao na classe Biblioteca

--- src/service/BibliotecaService.java
+++ src/service/BibliotecaService.java
@@ -1,5 +1,24 @@
 package service;
 
+import model.Livro;
+import model.Usuario;
+
 public class BibliotecaService {
-    
+
+    public void realizarEmprestimo(Livro livro, Usuario usuario) {
+        if (livro.isDisponivel()) {
+            livro.setDisponivel(false);
+            System.out.println(
+                "Sistema: Livro " + livro.getTitulo()
+                + " foi alugado para o usuario "
+                + usuario.getNome()
+                + " com sucesso!"
+            );
+        } else {
+            System.out.println(
+                "Sistema: O livro " + livro.getTitulo()
+                + " já está alugado."
+            );
+        }
+    }
 }

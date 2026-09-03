aaf67d32927368ba7e692c317a29d9c8c81ccc16
Gabriel Silva <gabriel.jrsilva007@gmail.com>
2026-09-02 21:09:23 -0300

    Adicionando as primeiras funcionalidades na classe App, feito importacao do package service e das classes livro, usuario e biblioteca

--- src/App.java
+++ src/App.java
@@ -1,7 +1,15 @@
+
+import java.util.Scanner;
+import model.Livro;
+import model.Usuario;
+import service.BibliotecaService;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner leitor = new Scanner(System.in);
+        BibliotecaService biblioteca = new BibliotecaService();
 
-        System.out.println("Meu primeiro programa em Java!");
+        // Criando os objetos iniciais 
+
     }
 }
